ecb025e5813e24ce46cc43d524663200f03e36fd
sbingley22 <sbingley22@gmail.com>
2023-10-15 15:09:34 +0100

    Elements module added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello")
+const Element = function(){
+    board = document.querySelector("#board")
+    tiles = [[],[],[]]
+    
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            tiles[i][j] = document.querySelector("#t"+String(i+j+1));      
+        }
+    }
+    
+    return{board, tiles}
+}
+
+const el = Element()
+console.log(el.tiles)
